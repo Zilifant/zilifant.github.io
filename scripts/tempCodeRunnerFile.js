c07989f74d31c64f6392e9d5b542d63cdde414f0
Scott Silsbe <scott.silsbe@gmail.com>
2020-12-28 17:19:34 -0500

    clean out all the junk

--- scripts/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-let arrayData = ["Sigit Prasetya","Indonesia","sigit@gmail.com"];
-    arrayData = ["Song Hye Kyo","South Korea","songsong@gmail.com"];
-    arrayData = ["Andy Lau","Hongkong","andyLau@gmail.com"];
-    arrayData = ["Enji Shaun","United State","shaun2@gmail.com"];
-    arrayData = ["Azumi","Japan","azumiK@gmail.com"];
-
-console.log(arrayData);
